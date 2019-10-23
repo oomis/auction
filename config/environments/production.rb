d83d6dbab5ea51e48314878ac7890357fba49499
oomis <damilolaomiyera@yahoo.com>
2019-10-23 19:21:24 +0100

    frontend fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,12 +5,12 @@ Rails.application.configure do
   config.cache_classes = true
 
 
-  config.action_mailer.default_url_options = { :host => 'andela-sales.herokuapp.com' }
+  # config.action_mailer.default_url_options = { :host => 'andela-sales.herokuapp.com' }
 
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "example.com",
+    domain: "andela-sales.herokuapp.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
