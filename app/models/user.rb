9dcf70ad12fe4ecaf2e0814655e5dcabb063edd5
oomis <damilolaomiyera@yahoo.com>
2019-10-23 22:02:47 +0100

    testing light google auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :masqueradable, :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
+  devise :masqueradable, :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  devise :omniauthable, omniauth_providers: [:google_oauth2]
   has_person_name
 
   has_many :notifications, foreign_key: :recipient_id
--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,8 @@ Rails.application.routes.draw do
 
   resources :notifications, only: [:index]
   resources :announcements, only: [:index]
-  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  # devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'auctions#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
