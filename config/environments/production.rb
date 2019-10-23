75f967beaba62a9592e601a353437cc6739f83ea
oomis <damilolaomiyera@yahoo.com>
2019-10-23 18:32:05 +0100

    frontend fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   config.cache_classes = true
 
 
-  config.action_mailer.default_url_options = { :host => 'andela-sales.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'andela-sales.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
