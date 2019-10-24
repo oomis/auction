63cfb69c1768adf13bfef6dbfbacb37887f990fa
oomis <damilolaomiyera@yahoo.com>
2019-10-24 08:53:31 +0100

    Revert "testing light google auth"
    This reverts commit 9dcf70ad12fe4ecaf2e0814655e5dcabb063edd5.

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider :google_oauth2, ENV['489680346267-6cardpnb43p66h3g4tnf6ervtrfkmkia.apps.googleusercontent.com'], ENV['FplynhHCZK69hbW1EdemXpzR']
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -27,8 +27,7 @@ Rails.application.routes.draw do
 
   resources :notifications, only: [:index]
   resources :announcements, only: [:index]
-  # devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   root to: 'auctions#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
